64e8db8b4088078e297fbd5ee92acf22a92d4cdf
nathen-win <rpriyo.uic.edu>
2025-04-07 22:16:35 -0500

    Made reusable input component for StateLogin

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Input from "./Input";
 
 export default function Login() {
   const [enteredValues, setEnteredValues] = useState({
@@ -11,6 +12,7 @@ export default function Login() {
   });
 
   const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const passwordIsInvalid = didEdit.password && enteredValues.password.trim().length < 6;
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -40,18 +42,24 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input 
-            id="password" 
-            type="password" 
-            name="password" 
-            onChange={() => handleInputChange("password", event.target.value)}
-            value={enteredValues.password}
-          />
-        </div>
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          onBlur={() => handleInputBlur('email')}
+          onChange={event => handleInputChange('email', event.target.value)}
+          value={enteredValues.email}
+          error={emailIsInvalid && 'Please enter a valid email!'}
+        />
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          onBlur={() => handleInputBlur('password')}
+          onChange={event => handleInputChange('password', event.target.value)}
+          value={enteredValues.password}
+          error={passwordIsInvalid && 'Please enter a valid password!'}
+        />
       </div>
 
       <p className="form-actions">
